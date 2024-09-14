e7a2375bc2685a8ceb9fe797477fd12e4d64100a
Ivan Shapoval <heysundi@yandex.ru>
2024-09-14 19:30:52 +0400

    '123'

---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sys import argv
+
+def readconfig():
+    results = []
+    with open('config.csv', 'r') as fread:
+        text = fread.read()
+        for line in text.split('\n'):
+            items = line.split(';')
+            results.append(items)
+    return results[1]
+
+
+def fun(n0, h, nk, a, b, c):
+    value = 0
+    for x in range(n0, nk, h):
+        value += a * (1 + (2 * x) / (np.exp(b * x) + c * c)) ** 0.5
+    return value
+
+
+def writeresult(value):
+    with open('results.csv', 'w') as fwrite:
+        fwrite.write(str(value))
+    return
+
+
+parameters = []
+parameters = readconfig()
+
+n0, h, nk, a, b, c = int(parameters[0]), \
+                     int(parameters[1]), \
+                     int(parameters[2]), \
+                     float(parameters[3]), \
+                     float(parameters[4]), \
+                     float(parameters[5])
+
+y = fun(n0, h, nk, a, b, c)
+
+writeresult(y)
